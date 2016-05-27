3e157c7ef9d755239d3a36790a691e037b892e7f
Simon Gladman <sgladman@gmail.com>
2016-05-27 14:00:23 +0100

    Initial Commit

---
+++ StrangeAttractor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StrangeAttractor
+//
+//  Created by Simon Gladman on 27/05/2016.
+//  Copyright © 2016 Simon Gladman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
